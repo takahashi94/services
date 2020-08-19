8ff2a5b19aa0add5a715bd626a548dc428b9e36e
ry_takahashi <ryosuke_takahashi@respawn.jp>
2020-08-19 15:31:19 +0900

    loguotのテスト

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 //ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
+//ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
